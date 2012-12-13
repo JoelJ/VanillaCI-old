53171635fae46870b50402b72ea6f3d2c2cf5a78
joeljohnson <joeljohnson@attask.com>
2012-12-12 21:05:47 -0700

    Removing unnecessary comment.

--- src/test/java/com/joelj/vanillaci/TestFiles.java
+++ src/test/java/com/joelj/vanillaci/TestFiles.java
@@ -10,17 +10,5 @@ import java.io.File;
 public class TestFiles {
 	public static File getResourceDirectory() {
 		return new File("src/test/resources");
-		// When running from IntelliJ, the working dir folder is different than Maven.
-		// This is probably a stupid workaround. But it works.
-//		File current = new File(".");
-//		String[] files = current.list();
-//		if(files != null) {
-//			for (String file : files) {
-//				if("src".equals(file)) {
-//					return new File("src/test/resources");
-//				}
-//			}
-//		}
-//		return new File("VanillaCI/src/test/resources");
 	}
 }
