b7c74f776ff094eff4b6fde0b6ee00b4625281e9
joeljohnson <joeljohnson@attask.com>
2013-01-01 18:39:19 -0700

    adding HeartbeatTasks for triggering and waiting for a job. They do nothing so far.

---
+++ SlaveMachine/src/main/java/com/joelj/vanillaci/heartbeat/TriggerJobHeartbeatItem.java
@@ -0,0 +1,41 @@
+package com.joelj.vanillaci.heartbeat;
+
+/**
+ * User: Joel Johnson
+ * Date: 1/1/13
+ * Time: 6:08 PM
+ */
+public class TriggerJobHeartbeatItem implements HeartbeatItem {
+	private final String id;
+	private final String jobName;
+
+	/**
+	 * Constructor used only for deserialization. Do not ever call this method.
+	 */
+	@Deprecated
+	TriggerJobHeartbeatItem() {
+		this.id = null;
+		this.jobName = null;
+	}
+
+	/**
+	 * @param id The id of this item. Must be globally unique.
+	 * @param name The name of the job to trigger.
+	 */
+	public TriggerJobHeartbeatItem(String id, String name) {
+		this.id = id;
+		this.jobName = name;
+	}
+
+	@Override
+	public String getId() {
+		return id;
+	}
+
+	/**
+	 * @return The name of the job that is being triggered.
+	 */
+	public String getName() {
+		return jobName;
+	}
+}
